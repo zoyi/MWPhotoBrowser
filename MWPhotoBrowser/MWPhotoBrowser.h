8ef71fa3e0529b9e5bb48fc7f07eb46efe1dc24e
Wooyoung Chung <yakle1218@gmail.com>
2017-03-17 12:00:10 +0900

    Restructed

---
+++ MWPhotoBrowser/MWPhotoBrowser.h
@@ -0,0 +1,19 @@
+//
+//  MWPhotoBrowser.h
+//  MWPhotoBrowser
+//
+//  Created by Haeun Chung on 17/03/2017.
+//  Copyright © 2017 ZOYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWPhotoBrowser.
+FOUNDATION_EXPORT double MWPhotoBrowserVersionNumber;
+
+//! Project version string for MWPhotoBrowser.
+FOUNDATION_EXPORT const unsigned char MWPhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWPhotoBrowser/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MWPhotoBrowser' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for MWPhotoBrowser
+  pod 'DACircularProgress', '~> 2.3'
+  pod 'SDWebImage', '~> 3.7', '!= 3.7.2'
+  pod 'MBProgressHUD', '~> 0.9'
+end
+
