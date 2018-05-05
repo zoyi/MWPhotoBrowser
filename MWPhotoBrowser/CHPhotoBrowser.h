4bcb16cece1e9159ed825807d0dc69551f91fc95
intoxicated <yakle1218@gmail.com>
2018-05-05 22:17:15 +0900

    Added cartfile

--- MWPhotoBrowser/CHPhotoBrowser.h
+++ MWPhotoBrowser/CHPhotoBrowser.h
@@ -9,11 +9,15 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for MWPhotoBrowser.
-FOUNDATION_EXPORT double MWPhotoBrowserVersionNumber;
+FOUNDATION_EXPORT double CHPhotoBrowserVersionNumber;
 
 //! Project version string for MWPhotoBrowser.
-FOUNDATION_EXPORT const unsigned char MWPhotoBrowserVersionString[];
+FOUNDATION_EXPORT const unsigned char CHPhotoBrowserVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MWPhotoBrowser/PublicHeader.h>
 
-
+#import <CHPhotoBrowser/MWPhotoBrowser.h>
+#import <CHPhotoBrowser/MWPhoto.h>
+#import <CHPhotoBrowser/UIImage+MWPhotoBrowser.h>
+#import <CHPhotoBrowser/MWCaptionView.h>
+#import <CHPhotoBrowser/MWPhotoProtocol.h>
